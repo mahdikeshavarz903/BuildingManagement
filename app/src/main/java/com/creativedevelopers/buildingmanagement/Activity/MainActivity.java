ea154b38f1a4ae93f448359847397095c7fcb97a
MMahdiMB <mmmmahdimmm@gmail.com>
2018-03-03 01:10:35 +0330

    Mohammad

--- app/src/main/java/com/creativedevelopers/buildingmanagement/Activity/MainActivity.java
+++ app/src/main/java/com/creativedevelopers/buildingmanagement/Activity/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //Mohammad Mahdi
         System.out.println("Hello world !!!");
 
         Building building=new Building();
