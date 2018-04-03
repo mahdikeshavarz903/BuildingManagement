fcdcda659b79b61d57ea6152864b17ea12ae77ba
Mohammad Mahdi <mmmmahdimmm@gmail.com>
2018-04-04 00:27:29 +0430

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/misc.xml

--- app/src/main/java/com/creativedevelopers/buildingmanagement/Unit_Test/Mohammad_Mahdi.java
+++ app/src/main/java/com/creativedevelopers/buildingmanagement/Unit_Test/Mohammad_Mahdi.java
@@ -4,10 +4,6 @@ package com.creativedevelopers.buildingmanagement.Unit_Test;
  * Created by Mahdi on 3/18/18.
  */
 
-public class Mohamad_Mahdi {
-    //Mohammad Mahdi
-    public static void Mohammad()
-    {
-//
-    }
+public class Mohammad_Mahdi {
+
 }
