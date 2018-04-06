325b75ba12180019e07dd443aa8473f5917ae62d
M@hdi <mahdikeshavarz903@yahoo.com>
2018-04-06 12:51:11 +0430

    update toolbar

--- app/src/main/java/com/creativedevelopers/buildingmanagement/Fragment/UnitsDetail.java
+++ app/src/main/java/com/creativedevelopers/buildingmanagement/Fragment/UnitsDetail.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,6 +27,12 @@ public class UnitsDetail extends android.app.Fragment
         super.onCreateView(inflater, container, savedInstanceState);
 
         view=inflater.inflate(R.layout.units_detail,null);
+
+        android.support.v7.widget.Toolbar toolbar=view.findViewById(R.id.unitsDetail_toolbar);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         return view;
     }
